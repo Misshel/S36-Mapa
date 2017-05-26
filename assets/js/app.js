92f1e78e82cff8981b5a9b8a0e6c0d9df5d27dab
Misshel <jenmish094@gmail.com>
2017-05-26 14:49:18 -0500

    trazo de ruta

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,12 +13,12 @@ function initMap() {
     }
   }
 
-  var latitud, longitud;
+  var latitud, longitud, miUbicacion;
   var funcionExito= function(position){
     latitud= position.coords.latitude;
     longitud= position.coords.longitude;
 
-    var miUbicacion= new google.maps.Marker({
+    miUbicacion= new google.maps.Marker({
       position: {lat:latitud, lng:longitud},
       map: map
     });
@@ -33,4 +33,49 @@ function initMap() {
   };
 
   document.getElementById("encuentrame").addEventListener("click",buscar);
+
+  var inputPartida = document.getElementById("partida");
+  var inputDestino = document.getElementById("destino");
+
+  new google.maps.places.Autocomplete(inputPartida);
+  new google.maps.places.Autocomplete(inputDestino);
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+
+  var calculateAndDisplayRoute = function (directionsService, directionsDisplay){
+    directionsService.route({
+      origin: inputPartida.value,
+      destination: inputDestino.value,
+      travelMode: 'DRIVING'
+    }, function(response, status){
+      if (status === 'OK'){
+        var distancia= Number((response.routes[0].legs[0].distance.text.replace("km","")).replace(",","."));
+        tarifa.classList.remove("none");
+        var costo = distancia * 1.75;
+        console.log(costo);
+        if (costo<4){
+          tarifa.innerHTML= "S/. 4";
+        }
+
+        tarifa.innerHTML= "S/. " + parseInt(costo);
+
+        console.log(response.routes[0].legs[0].distance.text);
+        directionsDisplay.setDirections(response);
+
+        miUbicacion.setMap(null);
+        console.log(miUbicacion);
+      } else {
+        window.alert("No encontramos una ruta.");
+      }
+    });
+
+  };
+  directionsDisplay.setMap(map);
+  var trazarRuta = function(){
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+
+  document.getElementById("trazar-ruta").addEventListener("click", trazarRuta);
+
 };
